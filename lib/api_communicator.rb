42618a83bf91d8a091edb5d509806560ea005fca
rkoko <rinda.ko@gmail.com>
2017-05-17 15:54:39 -0400

    anakin skywalker

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,13 +2,25 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
+def get_info
+  x=1
+  all =[]
+  while x < 10
+    all_characters= RestClient.get("http://www.swapi.co/api/people/?page=#{x}")
+    character_hash = JSON.parse(all_characters)
+    all.concat(character_hash["results"])
+    x+=1
+  end
+  return all
+end
+
+
 def get_character_movies_from_api(character)
   #make the web request
-
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
+  character_hash = get_info
   film_list = []
-    character_hash["results"].each do |char|
+    character_hash.each do |char|
       if character == char["name"].downcase
           char["films"].each do |url|
             film_list << url
@@ -28,11 +40,11 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
+
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
   titles = []
   films_hash.each do |url|
-      binding.pry
     films = RestClient.get(url)
     films_parsed =  JSON.parse(films)
     titles<<films_parsed["title"]
